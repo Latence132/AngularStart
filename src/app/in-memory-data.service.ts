9b05a4adbb5a711140e1080689ffa03490d9003a
Latence132 <alexandre.chaumet@gadz.org>
2020-06-06 11:48:16 +0200

    heroes are obj

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,19 +1,20 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero} from '../app/hero';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      new Hero(1, 'Narco'),
+      new Hero(2, 'Bombasto'),
+      new Hero(3, 'Bombasto'),
+      new Hero(4, 'Celeritas'),
+      new Hero(5, 'RubberMan'),
+      new Hero(6, 'Dynama'),
+      new Hero(7, 'Dr IQ'),
+      new Hero(8, 'Magma'),
+      new Hero(9, 'Tornado')
     ];
+    // myHero = this.heroes[0];
     return {heroes};
   }
 }
